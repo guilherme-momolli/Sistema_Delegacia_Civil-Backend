0456a7ffc6428f248b4319dd020c27c1d4104ba9
Guilherme Momolli <guilherme.momolli@gmail.com>
2025-07-22 14:47:59 -0300

    Dashboard Comentada

---
+++ src/main/java/br/gov/pr/pc/dp/sistema_delegacia_civil/controller/DashboardController.java
@@ -0,0 +1,19 @@
+//package br.gov.pr.pc.dp.sistema_delegacia_civil.controller;
+//
+//import br.gov.pr.pc.dp.sistema_delegacia_civil.service.DashboardService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//@RequiredArgsConstructor
+//@CrossOrigin
+//@RequestMapping("/dashboard")
+//public class DashboardController {
+//
+//    private final DashboardService dashboardService;
+//
+//
+//}
---
+++ src/main/java/br/gov/pr/pc/dp/sistema_delegacia_civil/service/DashboardService.java
@@ -0,0 +1,24 @@
+//package br.gov.pr.pc.dp.sistema_delegacia_civil.service;
+//
+//import br.gov.pr.pc.dp.sistema_delegacia_civil.dto.EstatisticasDashboardDTO;
+//import br.gov.pr.pc.dp.sistema_delegacia_civil.repository.ApreensaoRepository;
+//import br.gov.pr.pc.dp.sistema_delegacia_civil.repository.BoletimOcorrenciaRepository;
+//import br.gov.pr.pc.dp.sistema_delegacia_civil.repository.InqueritoPolicialRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class DashboardService {
+//
+//    private final BoletimOcorrenciaRepository boletimOcorrenciaRepository;
+//    private final InqueritoPolicialRepository inqueritoPolicialRepository;
+//    private final ApreensaoRepository apreensaoRepository;
+//
+////    public EstatisticasDashboardDTO gerarEstatisticas(){
+////        EstatisticasDashboardDTO dto = new EstatisticasDashboardDTO();
+////
+////    }
+//
+//
+//}
